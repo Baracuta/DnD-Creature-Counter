24d925793cbd5538fb305f1ef80030dfaaa66750
Benjamin Funk <lordbaracuta90@gmail.com>
2024-06-24 17:16:03 -0230

    Changing all the buttons to buttons

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-const add_button=
-document.getElementById("add_button");
-add_button.addEventListener("click", addCritter);
+let button=document.getElementById("add_button");
+button.addEventListener<"click">(addCritter);
 
 function addCritter(){
     document.getElementsByClassName(critterBox).style.backgroundColor="green";
