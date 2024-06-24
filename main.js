425715a60560d04b5bb90c2cc8a7d75ac1a1f212
Benjamin Funk <lordbaracuta90@gmail.com>
2024-06-24 18:13:20 -0230

    trying other things

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ document.getElementById("add_button");
 add_button.addEventListener("click",addCritter);
 
 function addCritter(){
-    document.getElementById(critterBox).style.backgroundColor="green";
+    document.getElementById(critterBox);
+    critterBox.style.backgroundColor="green";
 }
