7da203c3048222ddd39d85ad1748b08160204d51
Benjamin Funk <lordbaracuta90@gmail.com>
2024-06-25 14:28:52 -0230

    Trying to define the boxes, table, and button as variables in JS first

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-document.getElementById("add_button");
-let table=document.getElementById("creature_table");
-let box=document.getElementsByClassName("critterBox");
-add_button.addEventListener("click",addCritter);
+const jsBox=document.querySelectorAll("critterBox");
+
+const jsAdd=document.getElementById("add_button");
+
+const jsTable=document.querySelector("creature_table");
+
+//Old stuff here, currently not working
+//document.getElementById("add_button");
+//let table=document.getElementById("creature_table");
+//let box=document.getElementsByClassName("critterBox");
+
+jsAdd.addEventListener("click",addCritter);
 
 
 function addCritter(){
-    table.append(box);
+    jsTable.append(jsBox);
 }
