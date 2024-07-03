636d688c72621f94266239af78ca1a5e0f72ee5a
Benjamin Funk <lordbaracuta90@gmail.com>
2024-07-03 19:45:15 -0230

    Adding comments to keep track of javascript easier

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 //Global Variables
+//Get generic critterBox
 const jsBox=document.querySelectorAll("critterBox");
-
-const jsAdd=document.getElementById("add_button");
-
+//Get button that adds a new critterBox
+const jsAdd=document.getElementById("add_critter");
+//Get the table that holds all critterBox instances
 const jsTable=document.querySelector("creature_table");
 
-
+//Scripts that make Add Button Work
 
 function addCritter(){
     const newCritter=jsBox.cloneNode(true);
