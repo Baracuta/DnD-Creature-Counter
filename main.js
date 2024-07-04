a9b5ee62d1c9d83d58b9ba36a5077587b697ad50
Benjamin Funk <lordbaracuta90@gmail.com>
2024-07-03 21:45:03 -0230

    Oh my god it might almost be working

--- main.js
+++ main.js
@@ -4,10 +4,11 @@ const jsBox=document.querySelectorAll("critterBox");
 //Get button that adds a new critterBox
 const jsAdd=document.getElementById("add_critter");
 //Get the table that holds all critterBox instances
-const jsTable=document.querySelector("creature_table");
+const jsTable=document.getElementById("creature_table");
 
 //The things that add a new creature
 
 jsAdd.addEventListener("click", function(){
     const addBox=document.createElement("critterBox");
+    jsTable.appendChild(addBox);
 });
