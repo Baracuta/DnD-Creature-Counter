f6f904efc88b06d5aaa52c3c741afd7d390bf016
Benjamin Funk <lordbaracuta90@gmail.com>
2024-07-05 20:00:13 -0230

    Hoping this will make a maximum capacity of 12 boxes

--- main.js
+++ main.js
@@ -5,21 +5,25 @@ const jsBox=document.getElementsByTagName("genericCritter");
 const jsAdd=document.getElementById("add_critter");
 //Get the table that holds all critterBox instances
 const jsTable=document.getElementById("creature_table");
+//Variable that tracks the current number of critterBoxes
+let currentBoxes=0
 
 //Functions for Buttons
 //Add a new critter
 function addCritter(){
-    let newBox=document.createElement("div");
-        newBox.className="critterBox";
-        let newBoxname=document.createElement("input");
-            newBoxname.type="text";
-            newBoxname.className="critterName";
-        let newBoxhealth=document.createElement("input");
-            newBoxhealth.type="text";
-            newBoxhealth.className="critterHealth";
-        newBox.appendChild(newBoxname);
-        newBox.appendChild(newBoxhealth);
-    jsTable.appendChild(newBox);
+    if (currentBoxes<12);
+        let newBox=document.createElement("div");
+            newBox.className="critterBox";
+            let newBoxname=document.createElement("input");
+                newBoxname.type="text";
+                newBoxname.className="critterName";
+            let newBoxhealth=document.createElement("input");
+                newBoxhealth.type="text";
+                newBoxhealth.className="critterHealth";
+            newBox.appendChild(newBoxname);
+            newBox.appendChild(newBoxhealth);
+        jsTable.appendChild(newBox);
+        let currentBoxes=(currentBoxes+1);
 }
 
 //Button Listeners
