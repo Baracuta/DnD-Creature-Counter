d754f9d7628261ca92f4888684aa55817219650b
Benjamin Funk <lordbaracuta90@gmail.com>
2024-07-05 15:48:07 -0230

    Apparently, "newBox.createElement" was not a function

--- main.js
+++ main.js
@@ -11,8 +11,10 @@ const jsTable=document.getElementById("creature_table");
 function addCritter(){
     let newBox=document.createElement("div");
         newBox.className="critterBox";
-        let newBoxname=newBox.createElement("input type=text");
-        let newBoxhealth=newBox.createElement("input type=text");
+        let newBoxname=document.createElement("input type=text");
+        let newBoxhealth=document.createElement("input type=text");
+        newBox.appendChild(newBoxname);
+        newBox.appendChild(newBoxhealth);
     jsTable.appendChild(newBox);
 }
 
