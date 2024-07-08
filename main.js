e05869348df804dcbe409d07c030b8e75db62a6d
Benjamin Funk <lordbaracuta90@gmail.com>
2024-07-07 22:57:28 -0230

    ProbaBLY GONNA return an error because of how the order of operations is there, but that's okay

--- main.js
+++ main.js
@@ -7,6 +7,19 @@ const jsAdd=document.getElementById("add_critter");
 const jsTable=document.getElementById("creature_table");
 //Variable that tracks the current number of critterBoxes
 let currentBoxes=0
+//Variables for deleteButton IDs
+let delete1=document.getElementById("deleteButton1");
+let delete2=document.getElementById("deleteButton2");
+let delete3=document.getElementById("deleteButton3");
+let delete4=document.getElementById("deleteButton4");
+let delete5=document.getElementById("deleteButton5");
+let delete6=document.getElementById("deleteButton6");
+let delete7=document.getElementById("deleteButton7");
+let delete8=document.getElementById("deleteButton8");
+let delete9=document.getElementById("deleteButton9");
+let delete10=document.getElementById("deleteButton10");
+let delete11=document.getElementById("deleteButton11");
+let delete12=document.getElementById("deleteButton12");
 
 //Functions for Buttons
 //Add a new critter
@@ -49,3 +62,41 @@ function addCritter(){
 
 //Button Listeners
 jsAdd.addEventListener("click", addCritter);
+//Delete Button Functionality
+delete1.addEventListener("click",function(){
+    jsTable.removeChild(newBox1);
+});
+delete2.addEventListener("click",function(){
+    jsTable.removeChild(newBox2);
+});
+delete3.addEventListener("click",function(){
+    jsTable.removeChild(newBox3);
+});
+delete4.addEventListener("click",function(){
+    jsTable.removeChild(newBox4);
+});
+delete5.addEventListener("click",function(){
+    jsTable.removeChild(newBox5);
+});
+delete6.addEventListener("click",function(){
+    jsTable.removeChild(newBox6);
+});
+delete7.addEventListener("click",function(){
+    jsTable.removeChild(newBox7);
+});
+delete8.addEventListener("click",function(){
+    jsTable.removeChild(newBox8);
+});
+delete9.addEventListener("click",function(){
+    jsTable.removeChild(newBox9);
+});
+delete10.addEventListener("click",function(){
+    jsTable.removeChild(newBox10);
+});
+delete11.addEventListener("click",function(){
+    jsTable.removeChild(newBox11);
+});
+delete12.addEventListener("click",function(){
+    jsTable.removeChild(newBox12);
+});
+
