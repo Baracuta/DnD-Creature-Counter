9e2623190f3089c511dbc8bc8e59df1366d9e6a0
Benjamin Funk <lordbaracuta90@gmail.com>
2024-07-08 15:12:24 -0230

    Trying to fix something once again

--- main.js
+++ main.js
@@ -49,53 +49,64 @@ function addCritter(){
 }
 
 //Function to Delete Boxs
-function deleteButtons(e){
+function deleteButtons(newDeleteButton){
     document.getElementsByClassName("critterBox");
-    console.log(e);
+    console.log(newDeleteButton.id);
     if (newDeleteButton.id="deleteButton1"){
         alert("Button for box1");
         currentBoxes=(currentBoxes-1);
     }
+    else
     if (newDeleteButton.id="deleteButton2"){
         alert("Button for box2");
         currentBoxes=(currentBoxes-1);
     }
+    else
     if (newDeleteButton.id="deleteButton3"){
         alert("Button for box3");
         currentBoxes=(currentBoxes-1);
     }
+    else
     if (newDeleteButton.id="deleteButton4"){
         jsTable.removeChild(document.getElementById("critterBox4"));
         currentBoxes=(currentBoxes-1);
     }
+    else
     if (newDeleteButton.id="deleteButton5"){
         jsTable.removeChild("critterBox5");
         currentBoxes=(currentBoxes-1);
     }
+    else
     if (newDeleteButton.id="deleteButton6"){
         jsTable.removeChild("critterBox6");
         currentBoxes=(currentBoxes-1);
     }
+    else
     if (newDeleteButton.id="deleteButton7"){
         jsTable.removeChild("critterBox7");
         currentBoxes=(currentBoxes-1);
     }
+    else
     if (newDeleteButton.id="deleteButton8"){
         jsTable.removeChild("critterBox8");
         currentBoxes=(currentBoxes-1);
     }
+    else
     if (newDeleteButton.id="deleteButton9"){
         jsTable.removeChild("critterBox9");
         currentBoxes=(currentBoxes-1);
     }
+    else
     if (newDeleteButton.id="deleteButton10"){
         jsTable.removeChild("critterBox10");
         currentBoxes=(currentBoxes-1);
     }
+    else
     if (newDeleteButton.id="deleteButton11"){
         jsTable.removeChild("critterBox11");
         currentBoxes=(currentBoxes-1);
     }
+    else
     if (newDeleteButton.id="deleteButton12"){
         jsTable.removeChild("critterBox12");
         currentBoxes=(currentBoxes-1);
