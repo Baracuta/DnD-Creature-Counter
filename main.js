742b6651cae6c8052e5a742a787a07680de5776c
Benjamin Funk <lordbaracuta90@gmail.com>
2024-07-08 19:39:18 -0230

    Cut out some useless code while I troubleshoot the function

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ function addCritter(){
                     newNotesButton.className="notesButton";
                     newBoxButtons.appendChild(newNotesButton);
                 let newDeleteButton=document.createElement("button");
-                    newDeleteButton.className="deleteButton";
+                    newDeleteButton.className="deleteButtonClass";
                     newDeleteButton.id="deleteButton"+currentBoxes;
                     newDeleteButton.addEventListener("click",deleteButtons);
                     newBoxButtons.appendChild(newDeleteButton);
@@ -48,68 +48,29 @@ function addCritter(){
     }
 }
 
-//Function to Delete Boxs
-function deleteButtons(button){
+//Function to Delete Boxes
+function deleteButtons(deleteButton){
+    let whichOne=event.currentTarget;
     document.getElementsByClassName("critterBox");
-    let x=button.id;
+    let x=whichOne.id;
     console.log(button.id);
-    if (button.id="deleteButton1"){
+    if (x="deleteButton1"){
         alert("Button for box1");
         currentBoxes=(currentBoxes-1);
     }
     else
-    if (button.id="deleteButton2"){
+    if (x="deleteButton2"){
         alert("Button for box2");
         currentBoxes=(currentBoxes-1);
     }
     else
-    if (newDeleteButton.id="deleteButton3"){
+    if (x="deleteButton3"){
         alert("Button for box3");
         currentBoxes=(currentBoxes-1);
     }
     else
-    if (button.id="deleteButton4"){
+    if (x="deleteButton4"){
         jsTable.removeChild(document.getElementById("critterBox4"));
-        currentBoxes=(currentBoxes-1);
-    }
-    else
-    if (newDeleteButton.id="deleteButton5"){
-        jsTable.removeChild("critterBox5");
-        currentBoxes=(currentBoxes-1);
-    }
-    else
-    if (newDeleteButton.id="deleteButton6"){
-        jsTable.removeChild("critterBox6");
-        currentBoxes=(currentBoxes-1);
-    }
-    else
-    if (newDeleteButton.id="deleteButton7"){
-        jsTable.removeChild("critterBox7");
-        currentBoxes=(currentBoxes-1);
-    }
-    else
-    if (newDeleteButton.id="deleteButton8"){
-        jsTable.removeChild("critterBox8");
-        currentBoxes=(currentBoxes-1);
-    }
-    else
-    if (newDeleteButton.id="deleteButton9"){
-        jsTable.removeChild("critterBox9");
-        currentBoxes=(currentBoxes-1);
-    }
-    else
-    if (newDeleteButton.id="deleteButton10"){
-        jsTable.removeChild("critterBox10");
-        currentBoxes=(currentBoxes-1);
-    }
-    else
-    if (newDeleteButton.id="deleteButton11"){
-        jsTable.removeChild("critterBox11");
-        currentBoxes=(currentBoxes-1);
-    }
-    else
-    if (newDeleteButton.id="deleteButton12"){
-        jsTable.removeChild("critterBox12");
         currentBoxes=(currentBoxes-1);
     }
 }
