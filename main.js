3d94f806f0116433e9b865f668562b785871b8c4
Benjamin Funk <lordbaracuta90@gmail.com>
2024-07-08 00:37:18 -0230

    Still trying to get this working

--- main.js
+++ main.js
@@ -50,66 +50,54 @@ function addCritter(){
 
 //Function to Delete Boxs
 function deleteButtons(newDeleteButton){
-    let jsTable=document.getElementById("creature_table");
+    document.getElementsByClassName("critterBox");
     if (newDeleteButton.id="deleteButton1"){
-        jsTable.removeChild("critterBox1");
+        jsTable.removeChild(id="critterBox1");
         currentBoxes=(currentBoxes-1);
-        let jsTable=document.getElementById("creature_table");
     }
     if (newDeleteButton.id="deleteButton2"){
-        jsTable.removeChild("critterBox2");
+        jsTable.removeChild(id="critterBox2");
         currentBoxes=(currentBoxes-1);
-        let jsTable=document.getElementById("creature_table");
     }
     if (newDeleteButton.id="deleteButton3"){
-        jsTable.removeChild("critterBox3");
+        jsTable.removeChild(id="critterBox3");
         currentBoxes=(currentBoxes-1);
-        let jsTable=document.getElementById("creature_table");
     }
     if (newDeleteButton.id="deleteButton4"){
         jsTable.removeChild("critterBox4");
         currentBoxes=(currentBoxes-1);
-        let jsTable=document.getElementById("creature_table");
     }
     if (newDeleteButton.id="deleteButton5"){
         jsTable.removeChild("critterBox5");
         currentBoxes=(currentBoxes-1);
-        let jsTable=document.getElementById("creature_table");
     }
     if (newDeleteButton.id="deleteButton6"){
         jsTable.removeChild("critterBox6");
         currentBoxes=(currentBoxes-1);
-        let jsTable=document.getElementById("creature_table");
     }
     if (newDeleteButton.id="deleteButton7"){
         jsTable.removeChild("critterBox7");
         currentBoxes=(currentBoxes-1);
-        let jsTable=document.getElementById("creature_table");
     }
     if (newDeleteButton.id="deleteButton8"){
         jsTable.removeChild("critterBox8");
         currentBoxes=(currentBoxes-1);
-        let jsTable=document.getElementById("creature_table");
     }
     if (newDeleteButton.id="deleteButton9"){
         jsTable.removeChild("critterBox9");
         currentBoxes=(currentBoxes-1);
-        let jsTable=document.getElementById("creature_table");
     }
     if (newDeleteButton.id="deleteButton10"){
         jsTable.removeChild("critterBox10");
         currentBoxes=(currentBoxes-1);
-        let jsTable=document.getElementById("creature_table");
     }
     if (newDeleteButton.id="deleteButton11"){
         jsTable.removeChild("critterBox11");
         currentBoxes=(currentBoxes-1);
-        let jsTable=document.getElementById("creature_table");
     }
     if (newDeleteButton.id="deleteButton12"){
         jsTable.removeChild("critterBox12");
         currentBoxes=(currentBoxes-1);
-        let jsTable=document.getElementById("creature_table");
     }
 }
 
