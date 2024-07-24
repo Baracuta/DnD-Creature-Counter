2d95cd404ac8092b4f5242dd831117fdd6a03901
Benjamin Funk <lordbaracuta90@gmail.com>
2024-07-24 20:04:16 -0230

    Okayyy, added a lot here, just to see how it works. Obtained online, but I'll figure it out through messing with it.

--- main.js
+++ main.js
@@ -94,3 +94,73 @@ function unlockBoxes(){
 jsAdd.addEventListener("click", addCritter);
 jsUnlock.addEventListener("click", unlockBoxes);
 jsLock.addEventListener("click", lockBoxes);
+
+//Going to test all of the draggable content down here
+function handleDragStart(e) {
+    this.style.opacity = '0.4';
+  }
+  
+  function handleDragEnd(e) {
+    this.style.opacity = '1';
+  }
+  
+  let items = document.querySelectorAll('critterBox');
+  items.forEach(function (item) {
+    item.addEventListener('dragstart', handleDragStart);
+    item.addEventListener('dragend', handleDragEnd);
+  });
+  function handleDragStart(e) {
+    this.style.opacity = '0.4';
+  }
+
+  function handleDragEnd(e) {
+    this.style.opacity = '1';
+
+    items.forEach(function (item) {
+      item.classList.remove('over');
+    });
+  }
+
+  function handleDragOver(e) {
+    e.preventDefault();
+    return false;
+  }
+
+  function handleDragEnter(e) {
+    this.classList.add('over');
+  }
+
+  function handleDragLeave(e) {
+    this.classList.remove('over');
+  }
+
+  items.forEach(function(item) {
+    item.addEventListener('dragstart', handleDragStart);
+    item.addEventListener('dragover', handleDragOver);
+    item.addEventListener('dragenter', handleDragEnter);
+    item.addEventListener('dragleave', handleDragLeave);
+    item.addEventListener('dragend', handleDragEnd);
+    item.addEventListener('drop', handleDrop);
+  });
+  function handleDrop(e) {
+    e.stopPropagation(); // stops the browser from redirecting.
+    return false;
+  };
+  function handleDragStart(e) {
+    this.style.opacity = '0.4';
+  
+    dragSrcEl = this;
+  
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('text/html', this.innerHTML);
+  };
+  function handleDrop(e) {
+    e.stopPropagation();
+  
+    if (dragSrcEl !== this) {
+      dragSrcEl.innerHTML = this.innerHTML;
+      this.innerHTML = e.dataTransfer.getData('text/html');
+    }
+  
+    return false;
+  };
